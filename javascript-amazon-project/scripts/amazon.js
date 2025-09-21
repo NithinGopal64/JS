09a9afca0ebf02544dec4f4f2e309716340c5eff
Nithin <nithingopal13@gmail.com>
2025-09-20 22:13:36 -0400

    checkout page

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,3 +1,5 @@
+import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
 let html= ``;
 products.forEach((product) => {
 html+= `<div class="product-container">
@@ -39,7 +41,7 @@ html+= `<div class="product-container">
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
@@ -54,33 +56,58 @@ html+= `<div class="product-container">
 
 document.querySelector('.products-grid').innerHTML= html;
 
+function cartQuantity(){
+  let total = 0;
+
+  cart.forEach(cartItem => {
+  total += cartItem.quantity;
+  });
+  document.querySelector('.cart-quantity').innerHTML=total;
+}
+
+function handlerAddToCart(button){
+  let timeoutID;
+  return function addToCart(){
+    const ID = button.dataset.productId;
+    
+    selectorQuantity=document.querySelector(`.js-quantity-selector-${ID}`);
+    let quantityIncrement=Number(selectorQuantity.value);
+    const added=document.querySelector(`.js-added-${ID}`);
+    added.classList.add('js-added-to-cart');
+
+    if(timeoutID){
+      clearTimeout(timeoutID);
+    }
+    timeoutID = setTimeout(()=>{
+      added.classList.remove('js-added-to-cart')
+    },2000);
+
+    
+    let exists =0;
+
+    cart.forEach((cartItem)=>{
+      if(cartItem.id===ID){
+          cartItem.quantity+=quantityIncrement;
+          exists=1;
+    }
+    })
+    if (!exists){
+      cart.push({id: ID, quantity:quantityIncrement});
+
+    }
+
+    cartQuantity();  
+};
+
+
+}
+
+
+
 let selectorQuantity=0;
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
-        button.addEventListener('click',()=>{
-              const ID = button.dataset.productId;
-              
-              selectorQuantity=document.querySelector(`.js-quantity-selector-${ID}`);
-              let quantityIncrement=Number(selectorQuantity.value);
-              
-              let exists =0;
-           
-              cart.forEach((item)=>{
-                if(item.id===ID){
-                    item.quantity+=quantityIncrement;
-                    exists=1;
-              }
-              })
-              if (!exists){
-                cart.push({id: ID, quantity:quantityIncrement});
+        //let timeoutID;
+        button.addEventListener('click', handlerAddToCart(button));  
 
-              }
-            let total = 0;
-
-            cart.forEach(item => {
-            total += item.quantity;
-            });
-            document.querySelector('.cart-quantity').innerHTML=total;
-            
-        })
       
-})
+});
